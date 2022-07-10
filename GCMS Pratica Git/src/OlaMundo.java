728f2c6ab5db050c74eda9abd35d6187535815c7
lucasplucena <60987410+lucasplucena@users.noreply.github.com>
2022-07-10 19:45:30 -0300

    commit hotfix

--- GCMS Pratica Git/src/OlaMundo.java
+++ GCMS Pratica Git/src/OlaMundo.java
@@ -4,7 +4,7 @@ public class OlaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ola mundo");
-		System.out.println("modificacao hotfix");
+		System.out.println("modificacao hotfix na master-main");
 	}
 
 }
